17ee9605d3813dccbe88d02627f1f6f1f3aec70f
steffindhas177@gmail.com <146445015+steffindhas@users.noreply.github.com>
2024-01-28 17:59:10 +0530

    added

--- my-app/src/timeline/Timeline.js
+++ my-app/src/timeline/Timeline.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sugesstions from "./Sugesstions"
 import "./Timeline.css"
 import Post from './Post/Post'
 function Timeline() {
-  return (
+  const [posts, setPosts] = useState([
+    {
+      user: "ajay",
+      postImage: "img5.png",
+      likes: 23,
+      timestamp: "18h",
+    },
+    {
+      user: "abi",
+      postImage: "img1.png",
+      likes: 28,
+      timestamp: "16h",
+    },
+    {
+      user: "John",
+      postImage: "img4.png",
+      likes: 270,
+      timestamp: "1d",
+    },
+    {
+      user: "vijay",
+      postImage: "img2.png",
+      likes: 556,
+      timestamp: "2d",
+    },
+  ]);
+  return ( 
     <div className="timeline">
         <div className="timeline__left">
           <div className='timeline__post'>
-            <Post/>
+          {posts.map((post) => (
+            <Post
+              key={post.user} 
+              user={post.user}
+              postImage={post.postImage}
+              likes={post.likes}
+              timestamp={post.timestamp}
+            />
+          ))}
           </div>
         </div>
         <div className="timeline__right">
